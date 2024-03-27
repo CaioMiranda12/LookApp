720b86c17cdcd611848365d019b595fcdeee8ee2
CaioMiranda12 <caioclm04@gmail.com>
2024-03-27 00:03:42 -0300

    update commit

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ import {AppRegistry} from 'react-native';
 
 import {name as appName} from './app.json';
 import Home from './src/pages/Home';
+import SignUp from './src/pages/SignUp';
 
-AppRegistry.registerComponent(appName, () => Home);
+AppRegistry.registerComponent(appName, () => SignUp);
---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Box, Text, Title, Button, Spacer, Input} from '../../components';
+
+const SignUp = () => {
+  return (
+    <Box background="light" justify="center" align="center" hasPadding>
+      <Title>Create new account.</Title>
+      <Spacer />
+      <Text>Enter your details below:</Text>
+
+      <Spacer size="50px" />
+
+      <Input placeholder="Name" />
+      <Spacer />
+      <Input placeholder="E-mail" />
+      <Spacer />
+      <Input placeholder="Password" secureTextEntry />
+
+      <Spacer size="50px" />
+      <Button block>
+        <Text color="light">SignUp my account</Text>
+      </Button>
+      <Spacer size="20px" />
+      <Text underline onPress={() => alert('teste')}>
+        Create new account
+      </Text>
+    </Box>
+  );
+};
+
+export default SignUp;
